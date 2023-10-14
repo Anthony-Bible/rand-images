6a3d7a283058bc991151a9d63540ecb01f9f06dd
Anthony Bible <anthony@anthonybible.com>
2023-10-14 12:02:36 -0600

    use s3 for file storage

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/anthony-bible/rand-images
 
 go 1.21.1
+
+require github.com/aws/aws-sdk-go v1.45.25
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- main.go
+++ main.go
@@ -2,55 +2,95 @@ package main
 
 import (
     "fmt"
-    "io/ioutil"
     "math/rand"
     "net/http"
-    "os"
     "sync"
     "time"
+	"html/template"
+	"os"
+		"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	 "github.com/aws/aws-sdk-go/aws/credentials"
 )
 
 var (
     lastTwoImages []string
     mutex         sync.Mutex
+	s3Client *s3.S3
+	bucketName = "rand-images"
+	tmpl *template.Template
 )
 
 func init() {
     lastTwoImages = make([]string, 2)
     rand.Seed(time.Now().UnixNano()) // Seed the random number generator
+	 var err error
+    tmpl, err = template.New("").ParseFiles("./templates/image.html")
+    if err != nil {
+        fmt.Printf("Error parsing templates: %v", err)
+    }
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    files, err := ioutil.ReadDir("./images")
+	input := &s3.ListObjectsV2Input{
+		Bucket: aws.String(bucketName),
+	}
+	result, err  := s3Client.ListObjectsV2(input)
     if err != nil {
-        http.Error(w, "Unable to read the images directory", http.StatusInternalServerError)
+        http.Error(w, "Unable to list the images in the bucket", http.StatusInternalServerError)
         return
     }
 
-    if len(files) == 0 {
+    if len(result.Contents) == 0 {
         http.Error(w, "No images found", http.StatusNotFound)
         return
     }
-
-    var randomFile os.FileInfo
+    var randomObject *s3.Object
     mutex.Lock()
     for {
-        randomFile = files[rand.Intn(len(files))]
-        if randomFile.Name() != lastTwoImages[0] && randomFile.Name() != lastTwoImages[1] {
-            break
-        }
+		randomObject = result.Contents[rand.Intn(len(result.Contents))]
+		if *randomObject.Key != lastTwoImages[0] && *randomObject.Key != lastTwoImages[1] {
+			break
+		}
     }
 
     // Update lastTwoImages to remember the last two served images
-    lastTwoImages[0], lastTwoImages[1] = lastTwoImages[1], randomFile.Name()
-	fmt.Printf("using image %s", randomFile.Name())
+    lastTwoImages[0], lastTwoImages[1] = lastTwoImages[1], *randomObject.Key
+	fmt.Printf("using image %s\n", *randomObject.Key)
+	imageURL := fmt.Sprintf("https://photos.anthony.bible/file/%s/%s", bucketName, *randomObject.Key)
     mutex.Unlock()
+	//http.Redirect(w, r, urlStr, http.StatusTemporaryRedirect)
+	redirectParam := r.URL.Query().Get("redirect")
+    if redirectParam == "true" {
+        http.Redirect(w, r, imageURL, http.StatusSeeOther)
+        return
+    }
+	data:= struct {
+      ImageURL string
+	}{
+		ImageURL: imageURL,
+	}
 
-    http.ServeFile(w, r, fmt.Sprintf("./images/%s", randomFile.Name()))
+	err = tmpl.ExecuteTemplate(w, "image", data)
+      if err != nil {
+		fmt.Printf("Error rendering template: %v", err)  
+        http.Error(w, "Failed to render template", http.StatusInternalServerError)
+    }
 }
 
 func main() {
+	// Connect to backblaze
+	s3Config := &aws.Config{
+		Credentials: credentials.NewStaticCredentials(os.Getenv("IMAGE_ID"), os.Getenv("IMAGE_KEY"), ""),
+		Endpoint:  aws.String("https://s3.us-west-001.backblazeb2.com"),
+		Region: aws.String("us-west-001"),
+		S3ForcePathStyle: aws.Bool(true),
+	}
+	newSession := session.New(s3Config)
+	s3Client = s3.New(newSession)
     http.HandleFunc("/", handler)
+    
     http.ListenAndServe(":8080", nil)
 }
 
